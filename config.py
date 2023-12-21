ecf68d47182df6be0bfd863a53dcf3ffc222592d
DanilGoose <128906600+DanilGoose@users.noreply.github.com>
2023-12-21 18:44:40 +0700

    Update токен

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'TOKEN': '6467215549:AAF9Z3CsAnz32A7yKYo-HrWC6DuSZO39yRw',
+    'TOKEN': '6467215549:AAFmaEpwYcJP-jbd6O0XVY7YKPJlJgqY9Xg',
     'LOG_FILE': 'bot.log',
     'DOC_URL': 'https://docs.google.com/spreadsheets/d/1CaAC60WG1oydCELHNAKhplAuk7k_vx4GeEtyF8hiwXk/'
 } 
